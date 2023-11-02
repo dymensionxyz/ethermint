681361978cd0742861b95da877097655e1e96d49
Michael Tsitrin <michael@dymension.xyz>
2023-11-02 12:18:54 +0200

    handling StateDBCommitError

--- x/evm/keeper/state_transition.go
+++ x/evm/keeper/state_transition.go
@@ -180,6 +180,9 @@ func (k *Keeper) ApplyTransaction(ctx sdk.Context, msgEth *types.MsgEthereumTx) 
 	// pass true to commit the StateDB
 	res, err := k.ApplyMessageWithConfig(tmpCtx, msg, nil, true, cfg, txConfig)
 	if err != nil {
+		// when a transaction contains multiple msg, as long as one of the msg fails
+		// all gas will be deducted. so is not msg.Gas()
+		k.ResetGasMeterAndConsumeGas(ctx, ctx.GasMeter().Limit())
 		return nil, errorsmod.Wrap(err, "failed to apply ethereum core message")
 	}
 
